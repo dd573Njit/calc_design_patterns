60d6a36348f4d90775df603640a8a5f385031ebe
Dhananjay Dubey <dubeydhananjay9@gmail.com>
2024-03-09 16:46:09 -0500

    Initial commit for assignment6

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,38 @@
+import os
 import pkgutil
 import importlib
-from app.commands import CommandHandler
-from app.commands import Command
+import sys
+from app.commands import CommandHandler,Command
+from dotenv import load_dotenv
+import logging
+import logging.config
 
 class App:
     def __init__(self, dict):
+        os.makedirs('logs', exist_ok=True)
+        self.configure_logging()
+        load_dotenv()
+        self.settings = self.load_environment_variables()
+        self.settings.setdefault('ENVIRONMENT', 'PRODUCTION')
         self.command_handler = CommandHandler()
         self.dict = dict
         self.initialize_commands()
+        
+    def configure_logging(self):
+        logging_conf_path = 'logging.conf'
+        if os.path.exists(logging_conf_path):
+            logging.config.fileConfig(logging_conf_path, disable_existing_loggers=False)
+        else:
+            logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info("Logging configured.")
+
+    def load_environment_variables(self):
+        settings = {key: value for key, value in os.environ.items()}
+        logging.info("Environment variables loaded.")
+        return settings
+
+    def get_environment_variable(self, env_var: str = 'ENVIRONMENT'):
+        return self.settings.get(env_var, None)
 
     def initialize_commands(self):
         for cmd_name, cmd_instance in self.dict.items():
